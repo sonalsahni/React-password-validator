341acaf1a2e299da0e4ea0a13089612319aff016
sonalsahni <sonal.sahni20@gmail.com>
2017-01-24 16:02:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function Strength(props) {
+  const pass= props.value.toString();
+  const specialChar= new RegExp(/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/);
+  const num= new RegExp(/[0-9]/);
+  if(pass.length === 0){
+    return (<p> </p>);
+  }
+  if(pass.length < 6 ){
+    return(
+      <p className="weak">Too short</p>
+    )
+  }
+  if(!num.test(pass)) {
+      return (
+        <p className="med">Must contain atleast one number</p>
+      )
+    }
+  if(!specialChar.test(pass)) {
+      return (
+        <p className="med">Must contain atleast one special character</p>
+      )
+    }
+  return(
+    <div>
+      <p className="good">Strong password</p>
+      <input type="submit" value="Submit" />
+    </div>
+  )
+}
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={value:'',dis:"true"};
+    this.handleChange= this.handleChange.bind(this);
+    this.handleSubmit= this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your password was successfully subitted');
+    event.preventDefault();
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to My App</h2>
-        </div>
-        <p className="App-intro">
-          To start, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div >
+        <form onSubmit={this.handleSubmit} >
+        <input type="password" value={this.state.value} onChange={this.handleChange} />
+        <Strength value={this.state.value} />
+
+        </form>
+
       </div>
     );
   }
